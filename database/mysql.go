e7b371910c7d65bf6c3f846b3723bdacfc944d2c
xuexian song <s.xue.xian.c@gmail.com>
2021-01-18 15:30:19 +0800

    add mysql conn

---
+++ database/mysql.go
@@ -0,0 +1,72 @@
+package database
+
+import (
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/plugin/dbresolver"
+)
+
+type GormModel struct {
+	Id        int64 `gorm:"primaryKey;autoIncrement" json:"id" form:"id"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+type ResolverDSN struct {
+	Sources   []string
+	Replicas  []string
+	Secondary []string
+}
+
+var (
+	db *gorm.DB
+)
+
+func OpenDB(dsn ResolverDSN, config *gorm.Config, maxIdleConnes, maxOpenConnes int, models ...interface{}) (err error) {
+	if config == nil {
+		config = &gorm.Config{}
+	}
+
+	if db, err = gorm.Open(mysql.Open(dsn.Sources[0]), config); err != nil {
+		log.Errorf("opens database failed: %s", err.Error())
+		return
+	}
+
+	if err := db.Use(
+		dbresolver.Register(
+			dbresolver.Config{
+				Sources:  buildDialector(dsn.Sources),
+				Replicas: buildDialector(dsn.Replicas),
+				Policy:   dbresolver.RandomPolicy{},
+			},
+		).Register(
+			dbresolver.Config{Replicas: buildDialector(dsn.Secondary)},
+			"secondary",
+		).SetConnMaxIdleTime(time.Hour).
+			SetConnMaxLifetime(24 * time.Hour).
+			SetMaxIdleConns(maxIdleConnes).
+			SetMaxOpenConns(maxOpenConnes)); err != nil {
+		log.Errorf("opens database failed: %s", err.Error())
+		return
+	}
+
+	if err = db.AutoMigrate(models...); nil != err {
+		log.Errorf("auto migrate tables failed: %s", err.Error())
+	}
+	return
+}
+
+// 获取数据库链接
+func DB() *gorm.DB {
+	return db
+}
+
+func buildDialector(dsns []string) []gorm.Dialector {
+	var res []gorm.Dialector
+	for _, dsn := range dsns {
+		res = append(res, mysql.Open(dsn))
+	}
+	return res
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/enrichroad/community
+
+go 1.14
+
+require (
+	github.com/sirupsen/logrus v1.7.0
+	gorm.io/driver/mysql v1.0.3
+	gorm.io/gorm v1.20.11
+	gorm.io/plugin/dbresolver v1.1.0
+)
